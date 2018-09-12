4e24b50606fabef4a8c287591f1a3d7f45a54f85
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-09-12 18:30:51 +0530

    Create renderRecipe

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,3 +1,11 @@
 import { elements } from './base';
 
 export const getInput = () => elements.searchInput.value;
+
+const renderRecipe = recipes => {
+
+}
+
+export const renderResults = recipes => {
+  recipes.foreach(renderRecipe);
+}
