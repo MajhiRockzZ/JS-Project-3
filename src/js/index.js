89a10678ac76141868f7ce96ba8e78624dbf3f46
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-09-12 19:14:51 +0530

    Clear input field

--- src/js/index.js
+++ src/js/index.js
@@ -13,12 +13,13 @@ const state = {};
 const controlSearch = async () => {
   // 1) Get query from view
   const query = searchView.getInput();
-  
+
   if (query) {
     // 2) New search object and add to state
     state.search = new Search(query);
 
     // 3) Prepare UI for results
+    searchView.clearInput();
 
     // 4) Search for recipes
     await state.search.getResults();
