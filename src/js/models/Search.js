71e2a14a9855adc4296e0c19a1c5478ac43af381
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-09-12 16:30:39 +0530

    Build the search controller

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,19 @@
-export default 'I am an exported string.';
+import axios from 'axios';
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+  async getResults() {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = 'eb056f7767ddd06d7589ba50be536978';
+    try {
+      const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+      this.result = res.data.recipes;
+      // console.log(this.result);
+    } catch (error) {
+      alert(error);
+    }
+  }
+}
