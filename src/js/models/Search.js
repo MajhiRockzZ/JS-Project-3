0ee2e854917a17cb5d67d1816f84e92ffc5277a3
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-09-13 11:17:53 +0530

    Change api source to Edamam

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -7,10 +7,18 @@ export default class Search {
 
   async getResults() {
     const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const key = 'eb056f7767ddd06d7589ba50be536978';
+    // const key = 'eb056f7767ddd06d7589ba50be536978';
+    const appId = '688e660e';
+    const appKey = '6697b6714818a45958aedac392466716';
     try {
-      const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
-      this.result = res.data.recipes;
+      // const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+      const res = await axios(`https://api.edamam.com/search?q=${this.query}&app_id=${appId}&app_key=${appKey}`);
+      this.result = [];
+      this.recipeResult = res.data.hits.forEach(element => {
+        let recipeData = element.recipe;
+        this.result.push(recipeData);
+        // console.log(recipeData);
+      });
       // console.log(this.result);
     } catch (error) {
       alert(error);
