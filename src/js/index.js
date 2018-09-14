080db77139cd7e9318355de460920f72c22ca7e3
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-09-14 20:45:47 +0530

    Error point one

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global state of the app
@@ -67,6 +68,8 @@ const controlRecipe = async () => {
 
   if (id) {
     // Prepare UI for changes
+    recipeView.clearRecipe();
+    renderLoader(elements.recipe);
 
     // Create new recipe object
     state.recipe = new Recipe(id);
@@ -74,6 +77,7 @@ const controlRecipe = async () => {
     try {
       // Get recipe data and parse ingredient
       await state.recipe.getRecipe();
+      console.log(state.recipe.ingredients);
       state.recipe.parseIngredients();
 
       // Calculate servings and time
@@ -81,10 +85,12 @@ const controlRecipe = async () => {
       state.recipe.calcServings();
 
       // Render recipe
-      console.log(state.recipe);
+      clearLoader();
+      recipeView.renderRecipe(state.recipe);
     } catch (err) {
-      alert('Error processing recipe');
+      alert('Error processing recipe!');
+      console.log(err);
     }
   }
-}
-['hashchange', 'load'].forEach(event => window.addEventListener(event,controlRecipe));
+};
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
